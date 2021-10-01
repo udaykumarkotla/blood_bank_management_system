ed4124efe9212147652d5b45d7ab8409d6e6bd23
udaykumarkotla <77225205+udaykumarkotla@users.noreply.github.com>
2021-10-01 21:34:45 +0530

    remotesql

--- php/DBConnect.php
+++ php/DBConnect.php
@@ -2,10 +2,10 @@
 class DBConnect {
     private $db = NULL;
 
-    const DB_SERVER = "localhost";
-    const DB_USER = "root";
-    const DB_PASSWORD = "";
-    const DB_NAME = "donor";
+    const DB_SERVER = " remotemysql.com";
+    const DB_USER = "bP3zKgkOBe";
+    const DB_PASSWORD = "8WdRnE0GA8";
+    const DB_NAME = "bP3zKgkOBe";
 
     public function __construct() {
         $dsn = 'mysql:dbname=' . self::DB_NAME . ';host=' . self::DB_SERVER;
